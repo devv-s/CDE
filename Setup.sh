c6bb18bf25e8dce9e0a917eb81a9c229c2ad3a17
Dev Vrat Singh <itsdvvs.18@gmail.com>
2025-03-09 23:32:46 +0530

    attempt to use right /home/devvrat dir for config

--- Setup.sh
+++ Setup.sh
@@ -2,7 +2,6 @@
 
 NATIVE_PROGRAMS_SCRIPT="./Scripts/programs.native.sh"
 FLATPAK_PROGRAMS_SCRIPT="./Scripts/programs.flatpak.sh"
-USER_SHELL_CONFIGS="./Configs/.bashrc.d/"
 
 # ------------------------------------------------------
 # UTILITY FUNCTIONS FOR BETTER FLEXIBILITY AND CODE ORGANIZATION.
@@ -18,29 +17,43 @@ install_all_programs() {
 }
 
 set_default_user_configs() {
-  USER_SHELL_CONFIG_DIR="$HOME/.bashrc.d"
+  sudo -u  "$SUDO_USER" bash << EOF
+  echo 'executing as "$SUDO_USER"'
+  USER_SHELL_CONFIG_DIR="/home/$SUDO_USER/.bashrc.d"
+  echo 'User config dir: "$USER_SHELL_CONFIG_DIR"'
 
   if [[ -d "$USER_SHELL_CONFIG_DIR" ]]; then
+    echo 'user config dir: "$USER_SHELL_CONFIG_DIR"'
+
     # backing up the old configurations.
+    echo "User Config Directory already exists, backing up files..."
+
     for config_file in "$USER_SHELL_CONFIG_DIR"/*; do
       config_file_name=$(basename "$config_file")
       mv "$config_file" "$USER_SHELL_CONFIG_DIR"/"$config_file_name".bak
+      echo moved "$config_file" to "$USER_SHELL_CONFIG_DIR"/"$config_file_name".bak
     done
-
-    # and loading the default configurations.
-    config_dir="./Configs/bashrc.d"
-    if [[ -d "$config_dir" ]]; then
-      cp "$config_dir" "$HOME/.configs/"
-    fi
   fi
+
+  # and loading the default configurations.
+  SOURCE_CONFIG_DIR="./Configs/bashrc.d"
+
+  if [[ -d "$SOURCE_CONFIG_DIR" ]]; then
+    echo "found default configurations, setting them up..."
+    cp -R "$SOURCE_CONFIG_DIR" "$HOME/.configs/"
+    echo copied "$SOURCE_CONFIG_DIR" to "$HOME/.configs/"
+  fi
+
+EOF
 }
 
 # ------------------------------------------------------
 # PHASE: CONFIGURE ADDITIONAL REPOS, INSTALL PROGRAMS.
 # ------------------------------------------------------
-  install_all_programs
+install_all_programs
 
 # ------------------------------------------------------
 # PHASE: CONFIGURE USER ENV VARIABLES, CONFIGURATIONS.
 # ------------------------------------------------------
 set_default_user_configs
+
